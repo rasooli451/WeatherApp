89bca60f800e365eafc7d6dbcdbb8f55a4428356
Farhad Rasoli <rasoolyfarhad7@gmail.com>
2024-11-01 00:21:12 -0400

    Changed structure of City object, and added currenttime as a property

--- source/city.js
+++ source/city.js
@@ -8,17 +8,20 @@
 class City{
     weeklydata = [];
     hourlydata = [];
-    constructor(name, temp, weather){
+    constructor(name, temp, weather, humidity, windspeed, currtime){
         this.name = name;
         this.temp = temp;
         this.weather = weather;
+        this.humidity = humidity;
+        this.windspeed = windspeed;
+        this.currtime = currtime;
     }
 
-    addweeklydata(day, temp, weather) {
-        this.weeklydata.push({day, temp, weather})
+    addweeklydata(day, temp, maxtemp, mintemp, weather, humidity, windspeed) {
+        this.weeklydata.push({day, temp, maxtemp, mintemp, weather, humidity, windspeed})
     }
-    addhourlydata(time, temp, weather){
-        this.hourlydata.push({time, temp, weather});
+    addhourlydata(time, temp, weather, humidity, windspeed){
+        this.hourlydata.push({time, temp, weather, humidity, windspeed});
     }
 }
 
