b7c02e0de06ee689ba696f88da622b1f4c73286d
Farhad Rasoli <rasoolyfarhad7@gmail.com>
2024-11-04 00:34:52 -0500

    Added timezone offset to constructor parameters to find the time of searched city

--- source/city.js
+++ source/city.js
@@ -8,13 +8,14 @@
 class City{
     weeklydata = [];
     hourlydata = [];
-    constructor(name, temp, weather, humidity, windspeed, currtime){
+    constructor(name, temp, weather, humidity, windspeed, currtime, tzoffset){
         this.name = name;
         this.temp = temp;
         this.weather = weather;
         this.humidity = humidity;
         this.windspeed = windspeed;
         this.currtime = currtime;
+        this.tzoffset = tzoffset;
     }
 
     addweeklydata(day, temp, maxtemp, mintemp, weather, humidity, windspeed) {
